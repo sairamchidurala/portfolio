3b5c1d5996a3dc0145485f936f197731f5237410
Sairam Chidurala <sairam.chidurala@nextiva.com>
2025-08-21 14:24:39 +0530

    updated script to deploy to different envs

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# deploy.sh - Build & deploy Vite static site to EC2
-# Usage: ./deploy.sh <ec2-user> <ec2-ip> <remote-path>
-# Example: ./deploy.sh ubuntu 3.14.15.16 /var/www/sairam-bytes
+# deploy.sh [env]
+# Example: ./deploy.sh dev   → https://dev.iamsairam.in/
+#          ./deploy.sh       → https://iamsairam.in/
 
 EC2_USER=killer
 REMOTE_PATH=/var/www/sairam-bytes
+DOMAIN=iamsairam.in
+
+ENVIRONMENT=$1
+
+if [ "$ENVIRONMENT" == "dev" ]; then
+  REMOTE_PATH="${REMOTE_PATH}-dev"
+  DOMAIN="dev.${DOMAIN}"
+fi
 
 # 1️⃣ Install dependencies (if new packages)
 echo "Installing dependencies..."
@@ -20,4 +28,5 @@ rsync -avz --rsync-path="sudo rsync" dist/ $EC2_USER:$REMOTE_PATH
 
 # 4️⃣ Done
 echo "✅ Deployment complete!"
-echo "You can now open your site at https://iamsairam.in/"
+echo "You can now open your site at https://$DOMAIN/"
+
